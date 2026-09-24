c5f79cb4a7b56e44fc4e48c7bd6971199b6338d7
Naishatanha Agatha Balqisg41240059 <agathata0204@gmail.com>
2026-09-24 20:26:17 +0700

    update2

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -191,7 +191,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                 'Lupa password?',
                                 style: GoogleFonts.poppins(
                                   fontSize: 14,
-                                  color: const Color(0xFFFCEAEA),
+                                  color: const Color(0xFFF06292),
                                   fontWeight: FontWeight.w400,
                                 ),
                               ),
